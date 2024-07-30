de3ef1c7b4c3573e1e8c1892a3b289bf43c9283f
KO5YNIER <91388681+KA5ZTAN@users.noreply.github.com>
2024-07-31 00:30:08 +0200

    Add files via upload

---
+++ ai.py
@@ -0,0 +1,12 @@
+import time
+
+liczba = int(input("wpisz pelna liczbe: "))
+print("zbieranie danych...")
+time.sleep(3)
+print("przegladanie historii wyszukiwania...")
+time.sleep(2.5)
+print("analiza danych...")
+time.sleep(1.5)
+print("przetwarzanie danych...")
+time.sleep(1)
+print("twoja liczba to: ", liczba)
